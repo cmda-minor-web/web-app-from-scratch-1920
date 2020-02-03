aefa487ca987d41b072e3c98ba14a7ffbecc0b8b
Kris <kriskuiper74@gmail.com>
2020-02-03 14:41:28 +0100

    chore: run dev setup

---
+++ docs/scripts/app.js
@@ -0,0 +1,13 @@
+(function () {
+	'use strict';
+
+	const endpoint = 'https://api.spacexdata.com/v3/launches';
+
+	fetch(endpoint)
+		.then(response => response.json())
+		.then(console.log)
+		.catch(error => {
+			console.log('Could not fetch');
+		});
+
+}());
