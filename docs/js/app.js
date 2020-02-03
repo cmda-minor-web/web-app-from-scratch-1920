b027ceef9b695011e9901255f7ef6a78362e3db2
Kris <kriskuiper74@gmail.com>
2020-02-03 13:55:04 +0100

    feat: fetch and log Spacex api results

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,1 +1,8 @@
-console.log('works yes!')
+const endpoint = 'https://api.spacexdata.com/v3/launches'
+
+fetch(endpoint)
+	.then(response => response.json())
+	.then(console.log)
+	.catch(error => {
+		console.log('Could not fetch')
+	})
